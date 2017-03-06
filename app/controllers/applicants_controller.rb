8905eb18d9680531bfd4eea4b73b5852f5adb564
brandonseroyer <brandonseroyer@gmail.com>
2017-03-05 22:43:34 -0800

    correct syntax error

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -10,11 +10,13 @@ class ApplicantsController < ApplicationController
 
   def create
     @job = Job.find(params[:job_id])
-    @applicant = @job.applicants.new(applicant_params)\
+    @applicant = @job.applicants.new(applicant_params)
     if verify_recaptcha
       @applicant.save
       UserMailer.applicant_email.deliver
       redirect_to jobs_path, notice: 'Your Job Application Was Successfully Submitted.'
+    else
+      render :new
     end
   end
 
